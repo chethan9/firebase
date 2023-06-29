5046f1e7813843201d184bb00b8ee61ae11a8ba2
Chethan hn <chethan9@hotmail.com>
2023-06-29 16:06:09 +0300

    new endpoint

--- app.py
+++ app.py
@@ -69,10 +69,12 @@ def tor():
             data.append(row_dict)
         response = make_response(jsonify({"movies": data}), 200)
         response.headers["Content-Type"] = "application/json; charset=utf-8"
+        response.headers["X-Content-Type-Options"] = "nosniff"
         return response
     except Exception as e:
         response = make_response(jsonify({"error": str(e)}), 500)
         response.headers["Content-Type"] = "application/json; charset=utf-8"
+        response.headers["X-Content-Type-Options"] = "nosniff"
         return response
 
 
@@ -89,7 +91,8 @@ def magnet():
         if 'magnet:?xt=' in magnet_link:
             return magnet_link
         else:
-            return "Error: The link does not contain 'magnet:?xt='", 404
+            return "Error: The link does not contain 'magnet```python
+:?xt='", 404
     else:
         return "Error: Could not find the 'Open Magnet' button", 404
 
@@ -99,3 +102,58 @@ def parse():
     filename = request.args.get('filename')
     info = PTN.parse(filename)
     return jsonify(info)
+
+
+@app.route('/movie', methods=['GET'])
+def movie():
+    try:
+        # First, get the data from the /tor endpoint
+        name = request.args.get('name')
+        url = "https://2torrentz2eu.in/beta2/search.php?torrent-query=" + name
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        table = soup.find('table')
+        rows = table.find_all('tr')
+        data = []
+        for row in rows:
+            cols = row.find_all('td')
+            if len(cols) < 5:
+                continue  # Skip rows with fewer than 5 td elements
+            download_button = [col.find('button', class_='ui blue basic button') for col in cols]
+            download_link = []
+            for button in download_button:
+                if button:
+                    onclick_text = button.get('onclick')
+                    link = onclick_text.split("'")[1]
+                    full_link = "https://2torrentz2eu.in/beta2/page.php?url=" + link
+                    download_link.append(full_link)
+            # Remove empty strings from download_link
+            download_link = [link for link in download_link if link]
+            cols = [col.text.strip() for col in cols]
+            # Create a dictionary for each row
+            row_dict = {
+                "Title": cols[0],
+                "Seeds": int(cols[1]),
+                "Leeches": int(cols[2]),
+                "Size": cols[3],
+                "Date": cols[4],
+                "Download": download_link[0] if download_link else None
+            }
+            data.append(row_dict)
+        
+        # Then, for each title in the data, parse it with the /parse functionality
+        parsed_data = []
+        for movie in data:
+            title = movie['Title']
+            parsed_title = PTN.parse(title)
+            parsed_data.append(parsed_title)
+
+        response = make_response(jsonify({"movies": parsed_data}), 200)
+        response.headers["Content-Type"] = "application/json; charset=utf-8"
+        response.headers["X-Content-Type-Options"] = "nosniff"
+        return response
+    except Exception as e:
+        response = make_response(jsonify({"error": str(e)}), 500)
+        response.headers["Content-Type"] = "application/json; charset=utf-8"
+        response.headers["X-Content-Type-Options"] = "nosniff"
+        return response
